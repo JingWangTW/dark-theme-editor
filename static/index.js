3812fe8e84bc0e496dec033522ae0445124d0d0b
Jing Wang <tony.jing.wang@gmail.com>
2023-01-28 02:23:26 +0800

    responsive support

--- static/index.js
+++ static/index.js
@@ -28,6 +28,18 @@ function sidebarExpanding(e) {
     }
 }
 
+function showSidebar() {
+    document.querySelector("aside.sidebar").style.display = "block";
+    document.querySelector("aside.exapandable").style.display = "block";
+    document.querySelector("button.sidebar-toggle-btn").setAttribute("aria-expanded", true);
+}
+
+function hideSidebar() {
+    document.querySelector("aside.sidebar").style.display = "none";
+    document.querySelector("aside.exapandable").style.display = "none";
+    document.querySelector("button.sidebar-toggle-btn").setAttribute("aria-expanded", false);
+}
+
 window.addEventListener("load", function () {
     // sidebar expanble
     document.querySelector("aside.exapandable").addEventListener("mousedown", sidebarExpandStart);
@@ -36,6 +48,18 @@ window.addEventListener("load", function () {
     window.addEventListener("touchmove", sidebarExpanding);
     window.addEventListener("mouseup", sidebarExpandEnd);
     window.addEventListener("touchend", sidebarExpandEnd);
+
+    // responsive sidebar
+    document.querySelector("button.sidebar-toggle-btn").addEventListener("click", (event) => {
+        event.stopPropagation();
+        event.preventDefault();
+
+        if (event.currentTarget.getAttribute("aria-expanded") === "true") {
+            hideSidebar();
+        } else {
+            showSidebar();
+        }
+    });
 
     // sub dir toggle
     document.querySelectorAll("li.dir > span.dir-text").forEach((element) => {
