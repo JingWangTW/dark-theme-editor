4d4064eaff0f2fb8a770752c344afd93f79103a8
Jing Wang <tony.jing.wang@gmail.com>
2023-05-27 00:23:36 +0800

    feat: 🎸 Implement Codeblock Copy in JS

--- assets/index.js
+++ assets/index.js
@@ -55,6 +55,26 @@ function hideSidebar() {
 }
 
 window.addEventListener("load", function () {
+    document.querySelectorAll("button.copy-button").forEach((element) => {
+        element.addEventListener("click", (event) => {
+            let button = event.currentTarget;
+
+            if (button.getAttribute("state") === "copy") {
+                navigator.clipboard.writeText(
+                    decodeURI(button.getAttribute("data"))
+                );
+
+                button.setAttribute("state", "copied");
+                button.textContent = "Copied !";
+
+                window.setTimeout(() => {
+                    button.setAttribute("state", "copy");
+                    button.textContent = "Copy";
+                }, 3 * 1000);
+            }
+        });
+    });
+
     // sidebar expanble
     document
         .querySelector("aside.exapandable")
